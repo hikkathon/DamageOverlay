44f6d6761546286f70f648a58213abe7bbdcef56
hikkathon <hikkathon@gmail.com>
2021-02-20 02:28:19 +0700

    Размер окна

---
+++ WpfAppDPO/ConsoleTestWG.API/Account.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTestWG.API
+{
+    public class Meta
+    {
+        public int count { get; set; }
+    }
+
+    public class Clan
+    {
+        public int spotted { get; set; }
+        public int max_frags_tank_id { get; set; }
+        public int hits { get; set; }
+        public int frags { get; set; }
+        public int max_xp { get; set; }
+        public int max_xp_tank_id { get; set; }
+        public int wins { get; set; }
+        public int losses { get; set; }
+        public int capture_points { get; set; }
+        public int battles { get; set; }
+        public int damage_dealt { get; set; }
+        public int damage_received { get; set; }
+        public int max_frags { get; set; }
+        public int shots { get; set; }
+        public int frags8p { get; set; }
+        public int xp { get; set; }
+        public int win_and_survived { get; set; }
+        public int survived_battles { get; set; }
+        public int dropped_capture_points { get; set; }
+    }
+
+    public class Rating
+    {
+        public int spotted { get; set; }
+        public int calibration_battles_left { get; set; }
+        public int hits { get; set; }
+        public int frags { get; set; }
+        public int recalibration_start_time { get; set; }
+        public double mm_rating { get; set; }
+        public int wins { get; set; }
+        public int losses { get; set; }
+        public bool is_recalibration { get; set; }
+        public int capture_points { get; set; }
+        public int battles { get; set; }
+        public int current_season { get; set; }
+        public int damage_dealt { get; set; }
+        public int damage_received { get; set; }
+        public int shots { get; set; }
+        public int frags8p { get; set; }
+        public int xp { get; set; }
+        public int win_and_survived { get; set; }
+        public int survived_battles { get; set; }
+        public int dropped_capture_points { get; set; }
+    }
+
+    public class All
+    {
+        public int spotted { get; set; }
+        public int max_frags_tank_id { get; set; }
+        public int hits { get; set; }
+        public int frags { get; set; }
+        public int max_xp { get; set; }
+        public int max_xp_tank_id { get; set; }
+        public int wins { get; set; }
+        public int losses { get; set; }
+        public int capture_points { get; set; }
+        public int battles { get; set; }
+        public int damage_dealt { get; set; }
+        public int damage_received { get; set; }
+        public int max_frags { get; set; }
+        public int shots { get; set; }
+        public int frags8p { get; set; }
+        public int xp { get; set; }
+        public int win_and_survived { get; set; }
+        public int survived_battles { get; set; }
+        public int dropped_capture_points { get; set; }
+    }
+
+    public class Statistics
+    {
+        public Clan clan { get; set; }
+        public Rating rating { get; set; }
+        public All all { get; set; }
+        public object frags { get; set; }
+    }
+
+    public class Info
+    {
+        public Statistics statistics { get; set; }
+        public int account_id { get; set; }
+        public int created_at { get; set; }
+        public int updated_at { get; set; }
+        public object @private { get; set; }
+        public int last_battle_time { get; set; }
+        public string nickname { get; set; }
+    }
+
+    public class Data
+    {
+        public Info info { get; set; }
+    }
+
+    public class Account
+    {
+        public string status { get; set; }
+        public Meta meta { get; set; }
+        public Data data { get; set; }
+    }
+}
---
+++ WpfAppDPO/ConsoleTestWG.API/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+
+namespace ConsoleTestWG.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "WG API TEST";
+            while (true)
+            {
+                Task.Run(() => MainAsync());
+                Thread.Sleep(10000);
+                Console.Clear();
+            }
+        }
+
+        static async Task MainAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.wotblitz.ru/");
+                var content = new FormUrlEncodedContent(new[]
+                {
+                new KeyValuePair<string, string>("application_id", "8c4eecab18df2fd980424d5e35dba7bd"),
+                new KeyValuePair<string, string>("account_id", "71941826"),
+                new KeyValuePair<string, string>("extra", "statistics.rating")
+            });
+                var result = await client.PostAsync("/wotb/account/info/", content);
+                string json = await result.Content.ReadAsStringAsync();
+                int startIndex = json.IndexOf(@"""data"":{""") + 9;
+                int endIndex = json.LastIndexOf(@""":{""statistics""");
+                var str = json.Remove(startIndex, endIndex - startIndex).Insert(startIndex, "info");
+                var account = JsonConvert.DeserializeObject<Account>(str);
+                Console.WriteLine($"Ник: {account.data.info.nickname}\nБоёв: {account.data.info.statistics.all.battles}\nПобед: {account.data.info.statistics.all.wins}\nПоражений: {account.data.info.statistics.all.battles - account.data.info.statistics.all.wins}");
+            }
+        }
+    }
+}
